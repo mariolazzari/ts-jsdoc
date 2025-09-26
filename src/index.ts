4471714de4c80fd3989f1b60d64b86d880649acf
Mario Lazzari <mario.lazzari@gmail.com>
2025-09-26 08:44:47 +0200

    Done chapter 3 lesson 1

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
-import { Country } from "./Country";
-import { greetings } from "./script";
+/**
+ * Represents a country with its name and languages
+ * @template LanguagesType - The type for storing language data - can be a string or an array of strings
+ * @typedef {Object} Country
+ * @property {string} name - The full name of the country
+ * @property {LanguagesType} languages - languages spoken in the country. Can be either a comma-separated string or an array of strings
+ */
 
-console.log(greetings("Mario"));
+type Country<LanguagesType> = {
+  name: string;
+  languages: LanguagesType;
+};
 
-const italy = new Country("Italy", "IT");
-italy.addLanguage("Italian");
-console.log(italy);
+const nzLanguages: Country<string> = {
+  name: "New Zealand",
+  languages: "English, Maori",
+};
+
+const spainLanguages: Country<string[]> = {
+  name: "Spain",
+  languages: ["Spanish", "Catalan", "Galician", "Basque", "Valencian"],
+};
+
+console.log(nzLanguages);
+console.log(spainLanguages);
